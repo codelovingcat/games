f2646ac0986099cdd43c6a50c86d72063e45d467
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-01 16:59:58 +0200

    game-quiz

---
+++ games-quiz show/Quiz.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace games_quiz_show
+{
+    partial class Quiz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBitir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblYanlis = new System.Windows.Forms.Label();
+            this.lblDogru = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblEslestirme = new System.Windows.Forms.Label();
+            this.lblDogruCevap = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSoru = new System.Windows.Forms.Label();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnIleri = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBitir
+            // 
+            this.btnBitir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnBitir.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBitir.Location = new System.Drawing.Point(104, 137);
+            this.btnBitir.Name = "btnBitir";
+            this.btnBitir.Size = new System.Drawing.Size(96, 51);
+            this.btnBitir.TabIndex = 28;
+            this.btnBitir.Text = "Bitir";
+            this.btnBitir.UseVisualStyleBackColor = false;
+            this.btnBitir.Click += new System.EventHandler(this.btnBitir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(617, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 26);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Süre : ";
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblSayac.Location = new System.Drawing.Point(683, 31);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(30, 26);
+            this.lblSayac.TabIndex = 26;
+            this.lblSayac.Text = "60";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblYanlis);
+            this.groupBox1.Controls.Add(this.lblDogru);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(540, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 119);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Puan Tablosu";
+            // 
+            // lblYanlis
+            // 
+            this.lblYanlis.AutoSize = true;
+            this.lblYanlis.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblYanlis.Location = new System.Drawing.Point(74, 61);
+            this.lblYanlis.Name = "lblYanlis";
+            this.lblYanlis.Size = new System.Drawing.Size(21, 26);
+            this.lblYanlis.TabIndex = 14;
+            this.lblYanlis.Text = "0";
+            // 
+            // lblDogru
+            // 
+            this.lblDogru.AutoSize = true;
+            this.lblDogru.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblDogru.Location = new System.Drawing.Point(74, 29);
+            this.lblDogru.Name = "lblDogru";
+            this.lblDogru.Size = new System.Drawing.Size(21, 26);
+            this.lblDogru.TabIndex = 13;
+            this.lblDogru.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Yanlış : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 26);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Doğru : ";
+            // 
+            // lblEslestirme
+            // 
+            this.lblEslestirme.AutoSize = true;
+            this.lblEslestirme.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblEslestirme.Location = new System.Drawing.Point(13, 175);
+            this.lblEslestirme.Name = "lblEslestirme";
+            this.lblEslestirme.Size = new System.Drawing.Size(38, 13);
+            this.lblEslestirme.TabIndex = 24;
+            this.lblEslestirme.Text = "Soru : ";
+            // 
+            // lblDogruCevap
+            // 
+            this.lblDogruCevap.AutoSize = true;
+            this.lblDogruCevap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblDogruCevap.Location = new System.Drawing.Point(13, 143);
+            this.lblDogruCevap.Name = "lblDogruCevap";
+            this.lblDogruCevap.Size = new System.Drawing.Size(38, 13);
+            this.lblDogruCevap.TabIndex = 23;
+            this.lblDogruCevap.Text = "Soru : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 22);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Soru : ";
+            // 
+            // lblSoru
+            // 
+            this.lblSoru.AutoSize = true;
+            this.lblSoru.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSoru.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblSoru.Location = new System.Drawing.Point(72, 35);
+            this.lblSoru.Name = "lblSoru";
+            this.lblSoru.Size = new System.Drawing.Size(26, 22);
+            this.lblSoru.TabIndex = 21;
+            this.lblSoru.Text = "00";
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnD.Location = new System.Drawing.Point(372, 171);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(114, 60);
+            this.btnD.TabIndex = 20;
+            this.btnD.UseVisualStyleBackColor = false;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnC.Location = new System.Drawing.Point(239, 171);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(114, 60);
+            this.btnC.TabIndex = 19;
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnB.Location = new System.Drawing.Point(372, 101);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(114, 60);
+            this.btnB.TabIndex = 18;
+            this.btnB.UseVisualStyleBackColor = false;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnA.Location = new System.Drawing.Point(239, 101);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(114, 60);
+            this.btnA.TabIndex = 17;
+            this.btnA.UseVisualStyleBackColor = false;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnIleri
+            // 
+            this.btnIleri.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnIleri.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnIleri.Location = new System.Drawing.Point(104, 137);
+            this.btnIleri.Name = "btnIleri";
+            this.btnIleri.Size = new System.Drawing.Size(96, 51);
+            this.btnIleri.TabIndex = 16;
+            this.btnIleri.Text = "Başlat";
+            this.btnIleri.UseVisualStyleBackColor = false;
+            this.btnIleri.Click += new System.EventHandler(this.btnIleri_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(104, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(507, 74);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Quiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(763, 286);
+            this.Controls.Add(this.btnBitir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblEslestirme);
+            this.Controls.Add(this.lblDogruCevap);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSoru);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnIleri);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Quiz";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Quiz_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBitir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSayac;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblYanlis;
+        private System.Windows.Forms.Label lblDogru;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblEslestirme;
+        private System.Windows.Forms.Label lblDogruCevap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSoru;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnIleri;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ games-quiz show/Quiz.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace games_quiz_show
+{
+    public partial class Quiz : Form
+    {
+        int sayac = 60;
+        int question = 0;
+        int dogru = 0, yanlis = 0;
+        public Quiz()
+        {
+            InitializeComponent();
+            lblDogruCevap.Visible = false;
+            lblEslestirme.Visible = false;
+        }
+
+        private void btnIleri_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            btnA.Enabled = true;
+            btnB.Enabled = true;
+            btnD.Enabled = true;
+            btnC.Enabled = true;
+
+            btnA.BackColor = Color.LightGray;
+            btnB.BackColor = Color.LightGray;
+            btnC.BackColor = Color.LightGray;
+            btnD.BackColor = Color.LightGray;
+
+            btnIleri.Text = "İleri";
+            question++;
+            lblSoru.Text = question.ToString();
+            if (question == 1)
+            {
+                richTextBox1.Text = " 1) Mustafa Kemal Atatürk kaç yılında doğmuştur?";
+                btnA.Text = "1888";
+                btnB.Text = "1981";
+                btnC.Text = "1881";
+                btnD.Text = "1882";
+
+                lblDogruCevap.Text = "1881";
+            }
+            if (question == 2)
+            {
+                richTextBox1.Text = " 2) Anıtkabir hangi ilimizdedir?";
+                btnA.Text = "Ankara";
+                btnB.Text = "İstanbul";
+                btnC.Text = "İzmir";
+                btnD.Text = "Bursa";
+
+                lblDogruCevap.Text = "Ankara";
+            }
+            if (question == 3)
+            {
+                richTextBox1.Text = " 3) Türkiye de kaç coğrafi bölge vardır?";
+                btnA.Text = "15";
+                btnB.Text = "5";
+                btnC.Text = "10";
+                btnD.Text = "7";
+
+                lblDogruCevap.Text = "7";
+            }
+            if (question == 4)
+            {
+                richTextBox1.Text = " 4) Türkiye nin en kalabalık nüfusa sahip ili hangisidir?";
+                btnA.Text = "Ankara";
+                btnB.Text = "Konya";
+                btnC.Text = "Adana";
+                btnD.Text = "İstanbul";
+
+                lblDogruCevap.Text = "İstanbul";
+            }
+            if (question == 5)
+            {
+                richTextBox1.Text = " 5) İstiklal marşı kaç kıtadan oluşmaktadır?";
+                btnA.Text = "11";
+                btnB.Text = "10";
+                btnC.Text = "8";
+                btnD.Text = "12";
+
+                lblDogruCevap.Text = "10";
+
+                btnIleri.Enabled = false;
+                btnBitir.Visible = true;
+
+            }
+        }
+
+        private void Quiz_Load(object sender, EventArgs e)
+        {
+            btnBitir.Visible = false;
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            btnC.Enabled = false;
+            btnB.Enabled = false;
+            btnD.Enabled = false;
+            btnA.Enabled = false;
+
+
+            lblEslestirme.Text = btnA.Text;
+            if (lblEslestirme.Text == lblDogruCevap.Text)
+            {
+                timer1.Enabled = false;
+                sayac = sayac + 3;
+                lblSayac.Text = sayac.ToString();
+                btnA.BackColor = Color.Green;
+                richTextBox1.Text = " Cevabınız Doğru";
+                dogru++;
+                lblDogru.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                sayac = sayac - 3;
+                lblSayac.Text = sayac.ToString();
+                lblYanlis.Text = yanlis.ToString();
+                richTextBox1.Text = " Cevabınız Yanlış";
+                btnA.BackColor = Color.Red;
+            }
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnB.Enabled = false;
+
+
+            lblEslestirme.Text = btnB.Text;
+            if (lblEslestirme.Text == lblDogruCevap.Text)
+            {
+                timer1.Enabled = false;
+                sayac = sayac + 3;
+                lblSayac.Text = sayac.ToString();
+                btnB.BackColor = Color.Green;
+                richTextBox1.Text = " Cevabınız Doğru";
+                dogru++;
+                lblDogru.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                sayac = sayac - 3;
+                lblSayac.Text = sayac.ToString();
+                lblYanlis.Text = yanlis.ToString();
+                richTextBox1.Text = " Cevabınız Yanlış";
+                btnB.BackColor = Color.Red;
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            lblEslestirme.Text = btnC.Text;
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnD.Enabled = false;
+            btnC.Enabled = false;
+
+            if (lblEslestirme.Text == lblDogruCevap.Text)
+            {
+                timer1.Enabled = false;
+                sayac = sayac + 3;
+                lblSayac.Text = sayac.ToString();
+                btnC.BackColor = Color.Green;
+                richTextBox1.Text = " Cevabınız Doğru";
+                dogru++;
+                lblDogru.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                sayac = sayac - 3;
+                lblSayac.Text = sayac.ToString();
+                lblYanlis.Text = yanlis.ToString();
+                richTextBox1.Text = " Cevabınız Yanlış";
+                btnC.BackColor = Color.Red;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac = Convert.ToInt32(lblSayac.Text);
+
+            sayac = sayac - 1;
+            lblSayac.Text = sayac.ToString();
+            if (sayac == 0)
+            {
+                timer1.Enabled = false;
+                btnIleri.Enabled = false;
+                MessageBox.Show("Süre Bitti !!! ");
+            }
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnD.Enabled = false;
+            btnC.Enabled = false;
+
+            lblEslestirme.Text = btnD.Text;
+            if (lblEslestirme.Text == lblDogruCevap.Text)
+            {
+                timer1.Enabled = false;
+                sayac = sayac + 3;
+                lblSayac.Text = sayac.ToString();
+                btnD.BackColor = Color.Green;
+                richTextBox1.Text = " Cevabınız Doğru";
+                dogru++;
+                lblDogru.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                sayac = sayac - 3;
+                lblSayac.Text = sayac.ToString();
+                lblYanlis.Text = yanlis.ToString();
+                richTextBox1.Text = " Cevabınız Yanlış";
+                btnD.BackColor = Color.Red;
+            }
+        }
+
+        private void btnBitir_Click(object sender, EventArgs e)
+        {
+            Quiz form = new Quiz();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
