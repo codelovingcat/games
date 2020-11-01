fb2cfc38f42719a8be722b6e6c469f516423c90e
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-01 21:25:11 +0200

    games-zar

---
+++ games-dice/ZarGame.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace games_dice
+{
+    partial class ZarGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lbl2OyuncuPuan = new System.Windows.Forms.Label();
+            this.lbl1OyuncuPuan = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.picture2Zar2 = new System.Windows.Forms.PictureBox();
+            this.btnStart2 = new System.Windows.Forms.Button();
+            this.picture2Zar1 = new System.Windows.Forms.PictureBox();
+            this.lblToplam2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl2Zar2 = new System.Windows.Forms.Label();
+            this.lbl2Zar1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureZar2 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureZar1 = new System.Windows.Forms.PictureBox();
+            this.lblToplamPuan = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblZar2 = new System.Windows.Forms.Label();
+            this.lblZar1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2Zar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2Zar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnNewGame.Font = new System.Drawing.Font("Minion Pro Med", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNewGame.Location = new System.Drawing.Point(496, 271);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(84, 58);
+            this.btnNewGame.TabIndex = 13;
+            this.btnNewGame.Text = "Yeniden Oyna";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.White;
+            this.groupBox3.Controls.Add(this.lbl2OyuncuPuan);
+            this.groupBox3.Controls.Add(this.lbl1OyuncuPuan);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Font = new System.Drawing.Font("Minion Pro Med", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(631, 243);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(181, 115);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Puan Tablosu";
+            // 
+            // lbl2OyuncuPuan
+            // 
+            this.lbl2OyuncuPuan.AutoSize = true;
+            this.lbl2OyuncuPuan.Location = new System.Drawing.Point(127, 63);
+            this.lbl2OyuncuPuan.Name = "lbl2OyuncuPuan";
+            this.lbl2OyuncuPuan.Size = new System.Drawing.Size(15, 18);
+            this.lbl2OyuncuPuan.TabIndex = 3;
+            this.lbl2OyuncuPuan.Text = "0";
+            // 
+            // lbl1OyuncuPuan
+            // 
+            this.lbl1OyuncuPuan.AutoSize = true;
+            this.lbl1OyuncuPuan.Location = new System.Drawing.Point(127, 28);
+            this.lbl1OyuncuPuan.Name = "lbl1OyuncuPuan";
+            this.lbl1OyuncuPuan.Size = new System.Drawing.Size(15, 18);
+            this.lbl1OyuncuPuan.TabIndex = 2;
+            this.lbl1OyuncuPuan.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(34, 63);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 18);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "2. Oyuncu :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(34, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 18);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "1.Oyuncu :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.picture2Zar2);
+            this.groupBox2.Controls.Add(this.btnStart2);
+            this.groupBox2.Controls.Add(this.picture2Zar1);
+            this.groupBox2.Controls.Add(this.lblToplam2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.lbl2Zar2);
+            this.groupBox2.Controls.Add(this.lbl2Zar1);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Font = new System.Drawing.Font("Minion Pro Med", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(727, 24);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 199);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sen";
+            // 
+            // picture2Zar2
+            // 
+            this.picture2Zar2.Location = new System.Drawing.Point(144, 138);
+            this.picture2Zar2.Name = "picture2Zar2";
+            this.picture2Zar2.Size = new System.Drawing.Size(41, 38);
+            this.picture2Zar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture2Zar2.TabIndex = 2;
+            this.picture2Zar2.TabStop = false;
+            // 
+            // btnStart2
+            // 
+            this.btnStart2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart2.Location = new System.Drawing.Point(104, 94);
+            this.btnStart2.Name = "btnStart2";
+            this.btnStart2.Size = new System.Drawing.Size(62, 38);
+            this.btnStart2.TabIndex = 6;
+            this.btnStart2.Text = "Start";
+            this.btnStart2.UseVisualStyleBackColor = false;
+            this.btnStart2.Click += new System.EventHandler(this.btnStart2_Click);
+            // 
+            // picture2Zar1
+            // 
+            this.picture2Zar1.Location = new System.Drawing.Point(81, 138);
+            this.picture2Zar1.Name = "picture2Zar1";
+            this.picture2Zar1.Size = new System.Drawing.Size(40, 38);
+            this.picture2Zar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture2Zar1.TabIndex = 1;
+            this.picture2Zar1.TabStop = false;
+            // 
+            // lblToplam2
+            // 
+            this.lblToplam2.AutoSize = true;
+            this.lblToplam2.Location = new System.Drawing.Point(89, 61);
+            this.lblToplam2.Name = "lblToplam2";
+            this.lblToplam2.Size = new System.Drawing.Size(15, 18);
+            this.lblToplam2.TabIndex = 5;
+            this.lblToplam2.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Toplam Puan :";
+            // 
+            // lbl2Zar2
+            // 
+            this.lbl2Zar2.AutoSize = true;
+            this.lbl2Zar2.Location = new System.Drawing.Point(185, 26);
+            this.lbl2Zar2.Name = "lbl2Zar2";
+            this.lbl2Zar2.Size = new System.Drawing.Size(15, 18);
+            this.lbl2Zar2.TabIndex = 3;
+            this.lbl2Zar2.Text = "0";
+            // 
+            // lbl2Zar1
+            // 
+            this.lbl2Zar1.AutoSize = true;
+            this.lbl2Zar1.Location = new System.Drawing.Point(88, 26);
+            this.lbl2Zar1.Name = "lbl2Zar1";
+            this.lbl2Zar1.Size = new System.Drawing.Size(15, 18);
+            this.lbl2Zar1.TabIndex = 2;
+            this.lbl2Zar1.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(138, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 18);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "2. Zar :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 18);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "1. Zar :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.pictureZar2);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.pictureZar1);
+            this.groupBox1.Controls.Add(this.lblToplamPuan);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblZar2);
+            this.groupBox1.Controls.Add(this.lblZar1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Minion Pro Med", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(473, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 199);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ben";
+            // 
+            // pictureZar2
+            // 
+            this.pictureZar2.Location = new System.Drawing.Point(127, 138);
+            this.pictureZar2.Name = "pictureZar2";
+            this.pictureZar2.Size = new System.Drawing.Size(41, 38);
+            this.pictureZar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureZar2.TabIndex = 2;
+            this.pictureZar2.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart.Location = new System.Drawing.Point(76, 94);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 38);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureZar1
+            // 
+            this.pictureZar1.Location = new System.Drawing.Point(64, 138);
+            this.pictureZar1.Name = "pictureZar1";
+            this.pictureZar1.Size = new System.Drawing.Size(40, 38);
+            this.pictureZar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureZar1.TabIndex = 1;
+            this.pictureZar1.TabStop = false;
+            // 
+            // lblToplamPuan
+            // 
+            this.lblToplamPuan.AutoSize = true;
+            this.lblToplamPuan.Location = new System.Drawing.Point(94, 61);
+            this.lblToplamPuan.Name = "lblToplamPuan";
+            this.lblToplamPuan.Size = new System.Drawing.Size(15, 18);
+            this.lblToplamPuan.TabIndex = 5;
+            this.lblToplamPuan.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Toplam Puan :";
+            // 
+            // lblZar2
+            // 
+            this.lblZar2.AutoSize = true;
+            this.lblZar2.Location = new System.Drawing.Point(183, 25);
+            this.lblZar2.Name = "lblZar2";
+            this.lblZar2.Size = new System.Drawing.Size(15, 18);
+            this.lblZar2.TabIndex = 3;
+            this.lblZar2.Text = "0";
+            // 
+            // lblZar1
+            // 
+            this.lblZar1.AutoSize = true;
+            this.lblZar1.Location = new System.Drawing.Point(86, 25);
+            this.lblZar1.Name = "lblZar1";
+            this.lblZar1.Size = new System.Drawing.Size(15, 18);
+            this.lblZar1.TabIndex = 2;
+            this.lblZar1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Zar :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Zar :";
+            // 
+            // ZarGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::games_dice.Properties.Resources.dice;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(985, 382);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Name = "ZarGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ZarGame_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2Zar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2Zar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lbl2OyuncuPuan;
+        private System.Windows.Forms.Label lbl1OyuncuPuan;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox picture2Zar2;
+        private System.Windows.Forms.Button btnStart2;
+        private System.Windows.Forms.PictureBox picture2Zar1;
+        private System.Windows.Forms.Label lblToplam2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl2Zar2;
+        private System.Windows.Forms.Label lbl2Zar1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureZar2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureZar1;
+        private System.Windows.Forms.Label lblToplamPuan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblZar2;
+        private System.Windows.Forms.Label lblZar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ games-dice/ZarGame.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace games_dice
+{
+    public partial class ZarGame : Form
+    {
+        Random random = new Random();
+        int toplam1 = 0;
+        int toplam2 = 0;
+        public ZarGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStart2.Enabled = true;
+
+            int zar1 = random.Next(1, 7);
+            int zar2 = random.Next(1, 7);
+
+            if (zar1 == 1)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar1.png";
+            }
+            if (zar1 == 2)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\Dice-2.png";
+            }
+            if (zar1 == 3)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar3.png";
+            }
+            if (zar1 == 4)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice4.png";
+            }
+            if (zar1 == 5)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice-5.png";
+            }
+            if (zar1 == 6)
+            {
+                pictureZar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar6.png";
+            }
+
+            if (zar2 == 1)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar1.png";
+            }
+            if (zar2 == 2)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\Dice-2.png";
+            }
+            if (zar2 == 3)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar3.png";
+            }
+            if (zar2 == 4)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice4.png";
+            }
+            if (zar2 == 5)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice-5.png";
+            }
+            if (zar2 == 6)
+            {
+                pictureZar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar6.png";
+            }
+
+            lblZar1.Text = zar1.ToString();
+            lblZar2.Text = zar2.ToString();
+
+            toplam1 = toplam1 + zar1 + zar2;
+            lblToplamPuan.Text = toplam1.ToString();
+
+            lbl1OyuncuPuan.Text = toplam1.ToString();
+
+            if (toplam1 >= 100)
+            {
+                lblToplamPuan.Text = "Tebrikler Kazandın!!!";
+
+                btnStart.Enabled = false;
+                btnStart2.Enabled = false;
+                MessageBox.Show(toplam1 + " Puanla 1. Oyuncu Kazandı!!!.");
+                btnNewGame.Visible = true;
+            }
+        }
+
+        private void btnStart2_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStart2.Enabled = false;
+
+            int zar1 = random.Next(1, 7);
+            int zar2 = random.Next(1, 7);
+
+            if (zar1 == 1)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar1.png";
+            }
+            if (zar1 == 2)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\Dice-2.png";
+            }
+            if (zar1 == 3)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar3.png";
+            }
+            if (zar1 == 4)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice4.png";
+            }
+            if (zar1 == 5)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice-5.png";
+            }
+            if (zar1 == 6)
+            {
+                picture2Zar1.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar6.png";
+            }
+
+            if (zar2 == 1)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar1.png";
+            }
+            if (zar2 == 2)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\Dice-2.png";
+            }
+            if (zar2 == 3)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar3.png";
+            }
+            if (zar2 == 4)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice4.png";
+            }
+            if (zar2 == 5)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\dice-5.png";
+            }
+            if (zar2 == 6)
+            {
+                picture2Zar2.ImageLocation = @"C:\Users\urems\source\repos\games\games-dice\images\zar6.png";
+            }
+
+            lbl2Zar1.Text = zar1.ToString();
+            lbl2Zar2.Text = zar2.ToString();
+
+            toplam2 = toplam2 + zar2 + zar1;
+            lblToplam2.Text = toplam2.ToString();
+
+            lbl2OyuncuPuan.Text = toplam2.ToString();
+            if (toplam2 >= 100)
+            {
+                lblToplam2.Text = "Tebrikler Kazandın!!!";
+                btnStart.Enabled = false;
+                btnStart2.Enabled = false;
+
+                MessageBox.Show(toplam2 + " Puanla 2. Oyuncu Kazandı!!!.");
+                btnNewGame.Visible = true;
+            }
+        }
+
+        private void ZarGame_Load(object sender, EventArgs e)
+        {
+            btnNewGame.Visible = false;
+            btnStart.Enabled = true;
+            btnStart2.Enabled = false;
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            ZarGame form1 = new ZarGame();
+            form1.Show();
+            this.Hide();
+        }
+    }
+    
+}
