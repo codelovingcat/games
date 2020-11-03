ee0c2ac57a1ecea3b20ea1224c83a82a9c3b8863
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-03 23:47:04 +0200

    games-sudoku

---
+++ games-sudoku/Sudoku Oyna.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace games_sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNumber = new System.Windows.Forms.Button();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNumber
+            // 
+            this.btnNumber.BackColor = System.Drawing.Color.MediumOrchid;
+            this.btnNumber.Font = new System.Drawing.Font("Minion Pro SmBd", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNumber.Location = new System.Drawing.Point(50, 226);
+            this.btnNumber.Name = "btnNumber";
+            this.btnNumber.Size = new System.Drawing.Size(186, 48);
+            this.btnNumber.TabIndex = 19;
+            this.btnNumber.Text = "Sayıları Dağıt";
+            this.btnNumber.UseVisualStyleBackColor = false;
+            this.btnNumber.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // txt9
+            // 
+            this.txt9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txt9.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt9.Location = new System.Drawing.Point(178, 162);
+            this.txt9.Multiline = true;
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(58, 58);
+            this.txt9.TabIndex = 18;
+            this.txt9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt8
+            // 
+            this.txt8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txt8.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt8.Location = new System.Drawing.Point(114, 162);
+            this.txt8.Multiline = true;
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(58, 58);
+            this.txt8.TabIndex = 17;
+            this.txt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt7
+            // 
+            this.txt7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txt7.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt7.Location = new System.Drawing.Point(50, 162);
+            this.txt7.Multiline = true;
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(58, 58);
+            this.txt7.TabIndex = 16;
+            this.txt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt6
+            // 
+            this.txt6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txt6.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt6.Location = new System.Drawing.Point(178, 94);
+            this.txt6.Multiline = true;
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(58, 58);
+            this.txt6.TabIndex = 15;
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5
+            // 
+            this.txt5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.txt5.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt5.Location = new System.Drawing.Point(114, 94);
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(58, 58);
+            this.txt5.TabIndex = 14;
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.txt4.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt4.Location = new System.Drawing.Point(50, 94);
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(58, 58);
+            this.txt4.TabIndex = 13;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txt3.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt3.Location = new System.Drawing.Point(178, 27);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(58, 59);
+            this.txt3.TabIndex = 12;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txt2.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt2.Location = new System.Drawing.Point(113, 27);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(59, 59);
+            this.txt2.TabIndex = 11;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txt1.Font = new System.Drawing.Font("Minion Pro SmBd", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt1.Location = new System.Drawing.Point(50, 27);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(57, 59);
+            this.txt1.TabIndex = 10;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(288, 297);
+            this.Controls.Add(this.btnNumber);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumber;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
---
+++ games-sudoku/Sudoku Oyna.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace games_sudoku
+{
+    public partial class Form1 : Form
+    {
+        int[,] sudoku = new int[3, 3];
+        Random r = new Random();
+        int random = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sudoku[i, j] = 0;
+                }
+            }
+            random = r.Next(1, 4);
+            for (int f = 0; f < 3; f++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    while (ControlRows(sudoku, f, random) == false || ControlColumn(sudoku, i, random) == false)
+                    {
+                        random = r.Next(1, 4);
+                    }
+                    sudoku[f, i] = random;
+                }
+            }
+            txt1.Text = sudoku[0, 0].ToString();
+            txt2.Text = sudoku[0, 1].ToString();
+            txt3.Text = sudoku[0, 2].ToString();
+            txt4.Text = sudoku[1, 0].ToString();
+            txt5.Text = sudoku[1, 1].ToString();
+            txt6.Text = sudoku[1, 2].ToString();
+            txt7.Text = sudoku[2, 0].ToString();
+            txt8.Text = sudoku[2, 1].ToString();
+            txt9.Text = sudoku[2, 2].ToString();
+
+        }
+        public bool ControlRows(int[,] sudoku, int satir, int kontrol)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (sudoku[satir, i] == kontrol)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool ControlColumn(int[,] sudoku, int sutun, int kontrol)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (sudoku[i, sutun] == kontrol)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
