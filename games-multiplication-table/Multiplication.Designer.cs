89900c23c7b2c5c2623f96af3527a5b85e971c03
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-03 22:35:59 +0200

    games-multiplication

---
+++ games-multiplication-table/Multiplication.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace games_multiplication_table
+{
+    partial class Multiplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox10 = new System.Windows.Forms.ListBox();
+            this.listBox9 = new System.Windows.Forms.ListBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox10
+            // 
+            this.listBox10.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox10.FormattingEnabled = true;
+            this.listBox10.ItemHeight = 15;
+            this.listBox10.Location = new System.Drawing.Point(407, 207);
+            this.listBox10.Name = "listBox10";
+            this.listBox10.Size = new System.Drawing.Size(92, 154);
+            this.listBox10.TabIndex = 21;
+            // 
+            // listBox9
+            // 
+            this.listBox9.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox9.FormattingEnabled = true;
+            this.listBox9.ItemHeight = 15;
+            this.listBox9.Location = new System.Drawing.Point(309, 207);
+            this.listBox9.Name = "listBox9";
+            this.listBox9.Size = new System.Drawing.Size(92, 154);
+            this.listBox9.TabIndex = 20;
+            // 
+            // listBox8
+            // 
+            this.listBox8.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.ItemHeight = 15;
+            this.listBox8.Location = new System.Drawing.Point(211, 207);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(92, 154);
+            this.listBox8.TabIndex = 19;
+            // 
+            // listBox7
+            // 
+            this.listBox7.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.ItemHeight = 15;
+            this.listBox7.Location = new System.Drawing.Point(113, 207);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(92, 154);
+            this.listBox7.TabIndex = 18;
+            // 
+            // listBox6
+            // 
+            this.listBox6.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 15;
+            this.listBox6.Location = new System.Drawing.Point(15, 207);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(92, 154);
+            this.listBox6.TabIndex = 17;
+            // 
+            // listBox5
+            // 
+            this.listBox5.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 15;
+            this.listBox5.Location = new System.Drawing.Point(407, 41);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(92, 154);
+            this.listBox5.TabIndex = 16;
+            // 
+            // listBox4
+            // 
+            this.listBox4.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(309, 41);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(92, 154);
+            this.listBox4.TabIndex = 15;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(211, 41);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(92, 154);
+            this.listBox3.TabIndex = 14;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(113, 41);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(92, 154);
+            this.listBox2.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(15, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(92, 154);
+            this.listBox1.TabIndex = 12;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.Olive;
+            this.btnHesapla.Font = new System.Drawing.Font("Minion Pro SmBd", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.White;
+            this.btnHesapla.Location = new System.Drawing.Point(15, 8);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(484, 27);
+            this.btnHesapla.TabIndex = 11;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Multiplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(516, 370);
+            this.Controls.Add(this.listBox10);
+            this.Controls.Add(this.listBox9);
+            this.Controls.Add(this.listBox8);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Multiplication";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox10;
+        private System.Windows.Forms.ListBox listBox9;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ games-multiplication-table/Multiplication.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace games_multiplication_table
+{
+    public partial class Multiplication : Form
+    {
+        public Multiplication()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is ListBox)
+                {
+                    (item as ListBox).Items.Clear();
+                }
+            }
+
+            int number1, number2;
+
+            for (number1 = 1; number1 <= 10; number1++)
+            {
+                for (number2 = 1; number2 <= 10; number2++)
+                {
+                    int sonuc = number1 * number2;
+
+                    switch (number1)
+                    {
+                        case 1:
+                            listBox1.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 2:
+                            listBox2.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 3:
+                            listBox3.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 4:
+                            listBox4.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 5:
+                            listBox5.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 6:
+                            listBox6.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 7:
+                            listBox7.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 8:
+                            listBox8.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 9:
+                            listBox9.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        case 10:
+                            listBox10.Items.Add(number1 + " X " + number2 + " = " + sonuc);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
